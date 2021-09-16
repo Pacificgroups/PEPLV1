65e3837a7ef6343a9ff581b06305f3c6751a2313
Pacificgroups <Prashantranjan532@gmail.com>
2021-09-17 00:07:58 +0530

    Input Format A number n Output Format *	*	*	*	* 	*	*	*	* 		*	*	* 			*	* 				*

---
+++ Pattern4.java
@@ -0,0 +1,23 @@
+import java.util.*;
+public class Pattern4 {
+    public static void main(String[] args) {
+        Scanner scn = new Scanner(System.in);
+
+        int n=scn.nextInt();
+        int st=n;
+        int sp=0;
+
+        for(int i=1;i<=n;i++){
+            for(int j=0;j<sp;j++){
+                System.out.print("\t");
+            }
+            for(int k=1;k<=st;k++){
+                System.out.print("*\t");
+            }
+            st--;
+            sp++;
+            System.out.println();
+        }
+
+    }
+}
