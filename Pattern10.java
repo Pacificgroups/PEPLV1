04bf2658e8a9132f3cf556d48ffd052be4ec85a5
Pacificgroups <Prashantranjan532@gmail.com>
2021-09-17 01:40:23 +0530

    Input Format A number n Output Format 		* 	*		* *				* 	*		* 		*

---
+++ Pattern10.java
@@ -0,0 +1,36 @@
+import java.util.*;
+public class Pattern10 {
+    public static void main(String[] args) {
+        Scanner scn = new Scanner(System.in);
+
+        // write ur code here
+        int n=scn.nextInt();
+        int os=n/2;
+        int is=-1;
+
+        for(int i=1;i<=n;i++){
+            for(int j=1;j<=os;j++){
+                System.out.print("\t");
+            }
+            System.out.print("*\t");
+
+            for(int k=1;k<=is;k++){
+                System.out.print("\t");
+            }
+            if(i!=1 && i!=n){
+                System.out.print("*");
+            }
+
+
+            if(i<=n/2){
+                os--;
+                is+=2;
+            }else {
+                is-=2;
+                os++;
+            }
+            System.out.println();
+        }
+
+    }
+}
