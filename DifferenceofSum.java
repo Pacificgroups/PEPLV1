86f9a98442f855b74f9a74eef9c68aa05718147d
Pacificgroups <Prashantranjan532@gmail.com>
2021-09-27 02:56:48 +0530

    def differenceofSum(n. m)
    The function accepts two integers n, m as arguments Find the sum of all numbers in range from 1 to m(both inclusive) that are not divisible by n. Return difference between sum of integers not divisible by n with sum of numbers divisible by n.
    
    Assumption:
    
    n>0 and m>0
    Sum lies between integral range
    Example
    
    Input
    n:4
    m:20
    Output
    90
    
    Explanation
    
    Sum of numbers divisible by 4 are 4 + 8 + 12 + 16 + 20 = 60
    Sum of numbers not divisible by 4 are 1 +2 + 3 + 5 + 6 + 7 + 9 + 10 + 11 + 13 + 14 + 15 + 17 + 18 + 19 = 150
    Difference 150 – 60 = 90

---
+++ DifferenceofSum.java
@@ -0,0 +1,30 @@
+import java.util.Scanner;
+
+public class DifferenceofSum {
+    public static int Diff(int n,int m){
+        int sum1=0;
+        int sum2=0;
+
+        for(int i=1;i<=m;i++){
+            if(i%n==0){
+                sum1+=i;
+            }else{
+                sum2+=i;
+            }
+        }
+        return Math.abs(sum2-sum1);
+
+    }
+        public static void main (String[]args)
+        {
+            Scanner sc = new Scanner (System.in);
+            int n = sc.nextInt ();
+
+            int m=sc.nextInt();
+
+            System.out.println(Diff(n,m));
+
+
+        }
+
+}
